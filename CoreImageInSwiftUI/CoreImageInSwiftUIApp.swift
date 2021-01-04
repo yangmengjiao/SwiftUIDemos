09660e77b9c179b61540b363eb31209c628f4183
yangmengjiao <mengjiaoshun@gmail.com>
2021-01-04 16:53:35 -0600

    Initial Commit

---
+++ CoreImageInSwiftUI/CoreImageInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageInSwiftUIApp.swift
+//  CoreImageInSwiftUI
+//
+//  Created by mengjiao on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
